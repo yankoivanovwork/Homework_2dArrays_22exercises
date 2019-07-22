bf1ac0eaaf357e970fee45e01988721f466af65f
yankoivanovwork <51404578+yankoivanovwork@users.noreply.github.com>
2019-07-22 12:11:16 +0300

    22July
    zad20,zad21 done, zad22 fix

--- Homework_2dArrays_Zad20/Program.cs
+++ Homework_2dArrays_Zad20/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Linq;
 
 namespace Homework_2dArrays_Zad20
 {
@@ -7,50 +6,206 @@ namespace Homework_2dArrays_Zad20
     {
         static void Main(string[] args)
         {
-            //namefix - zad22*
-            int startNumber = 1;
-            int endNumber = 1000000;
-            long totalSum = ((long)endNumber * ((long)startNumber + (long)endNumber) / 2);
-            long missingNumber = 0;
-            long orderedArraySum = 0;
-            Random randomNumber = new Random();
+            int matrixRank = 0;
 
-            long[] orderedArray = new long[endNumber];
-            int randomGeneratedNumber = randomNumber.Next(startNumber, endNumber);
-
-            for (int i = startNumber; i <= endNumber; i++)
+            if (int.TryParse(Console.ReadLine(), out matrixRank) & matrixRank >= 2)
             {
-                if (i == randomGeneratedNumber)
+                int triangleIndex = -1;
+                int lowestNumber = int.MaxValue;
+                int numberCounter = 1;
+                int[,] matrixNumbers = new int[matrixRank, matrixRank];
+                int[,] resultTriangleInMatrix = FillMatrixWithZeros(matrixRank);
+
+                ///sumForEachTriangle index
+                ///0 nad glaven diagonal
+                ///1 pod glaven diagonal
+                ///2 nad glaven ogledalen diagonal
+                ///3 pod glaven ogledalen diagonal
+                int[] sumForEachTriangle = new int[4];
+
+                Console.WriteLine("Матрица: ");
+                for (int i = 0; i < matrixRank; i++)
                 {
-                    orderedArray[i - 1] = 0;
+                    for (int j = 0; j < matrixRank; j++)
+                    {
+                        Console.Write("Число " + numberCounter + ":");
+                        int.TryParse(Console.ReadLine(), out matrixNumbers[i, j]);
+                        numberCounter++;
+                    }
+                }
+
+                //sum za triangle nad glaven diagonal
+                for (int i = 0; i < matrixRank; i++)
+                {
+                    for (int j = 0; j < matrixRank - i; j++)
+                    {
+                        sumForEachTriangle[0] += matrixNumbers[j, j + i];
+                    }
+                }
+
+                //sum za triangle pod glaven diagonal
+                for (int i = 0; i < matrixRank; i++)
+                {
+                    for (int j = 0; j < matrixRank - i; j++)
+                    {
+                        sumForEachTriangle[1] += matrixNumbers[j + i, j];
+                    }
+                }
+
+                //sum za triangle nad glaven ogledalen diagonal
+                for (int i = matrixRank - 1; i >= 0; i--)
+                {
+                    for (int j = 0; j < (i + 1) ; j++)
+                    {
+                        sumForEachTriangle[2] += matrixNumbers[j, i - j];
+                    }
+                }
+
+                //sum za triangle pod glaven ogledalen diagonal
+                for (int i = matrixRank - 1; i >= 0; i--)
+                {
+                    for (int j = 0; j < (i + 1); j++)
+                    {
+                        if (i == 1)
+                        {
+                            sumForEachTriangle[3] += matrixNumbers[j + i, 2 * i - j];
+                            continue;
+                        }
+                        else if (i == 0)
+                        {
+                            sumForEachTriangle[3] += matrixNumbers[2, 2];
+                            continue;
+                        }
+
+                        sumForEachTriangle[3] += matrixNumbers[i - j, j];
+                    }
+                }
+
+                for (int i = 0; i < 4; i++)
+                {
+                    if (lowestNumber > sumForEachTriangle[i])
+                    {
+                        lowestNumber = sumForEachTriangle[i];
+                        triangleIndex = i;
+                    }
+                }
+
+                if (triangleIndex == 0)
+                {
+                    resultTriangleInMatrix = TriangleAboveMainDiagonal(matrixNumbers);
+                }
+                else if (triangleIndex == 1)
+                {
+                    resultTriangleInMatrix = TriangleBelowMainDiagonal(matrixNumbers);
+                }
+                else if (triangleIndex == 2)
+                {
+                    resultTriangleInMatrix = TriangleAboveReflectedMainDiagonal(matrixNumbers);
                 }
                 else
                 {
-                    orderedArray[i - 1] = i;
+                    resultTriangleInMatrix = TriangleBelowReflectedMainDiagonal(matrixNumbers);
+                }
+
+                for (int i = 0; i < matrixRank; i++)
+                {
+                    for (int j = 0; j < matrixRank; j++)
+                    {
+                        Console.Write(resultTriangleInMatrix[i, j] + " ");
+                    }
+                    Console.Write(Environment.NewLine);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Error!");
+            }
+        }
+
+        private static int[,] FillMatrixWithZeros(int matrixRank)
+        {
+            int[,] matrix = new int[matrixRank, matrixRank];
+
+            for (int i = 0; i < matrixRank; i++)
+            {
+                for (int j = 0; j < matrixRank; j++)
+                {
+                    matrix[i, j] = 0;
                 }
             }
 
-            Console.WriteLine("Total Sum of numbers 1 - 1 000 000 is: " + totalSum);
+            return matrix;   
+        }
 
-            Console.WriteLine("Sum() of the array with missing number is: " + orderedArray.Sum());
+        private static int[,] TriangleAboveMainDiagonal(int[,] matrixNumbers)
+        {
+            int[,] resultMatrix = new int[matrixNumbers.GetLength(0), matrixNumbers.GetLength(1)];
 
-            for (int i = 0; i < endNumber; i++)
+            for (int i = 0; i < matrixNumbers.GetLength(0); i++)
             {
-                orderedArraySum += orderedArray[i];
+                for (int j = 0; j < matrixNumbers.GetLength(1) - i; j++)
+                {
+                    resultMatrix[j, j + i] = matrixNumbers[j, j + i];
+                }
             }
 
-            Console.WriteLine("Manual sum of the array with missing number is: " + orderedArraySum);
+            return resultMatrix;
+        }
 
-            missingNumber = totalSum - orderedArraySum;
+        private static int[,] TriangleBelowMainDiagonal(int[,] matrixNumbers)
+        {
+            int[,] resultMatrix = new int[matrixNumbers.GetLength(0), matrixNumbers.GetLength(1)];
 
-            Console.WriteLine();
-
-            if (missingNumber == randomGeneratedNumber)
+            for (int i = 0; i < matrixNumbers.GetLength(0); i++)
             {
-                Console.WriteLine("Result from 'totalSum - arraySum' number is: " + missingNumber);
-
-                Console.WriteLine("Generated missing number is: " + randomGeneratedNumber);
+                for (int j = 0; j < matrixNumbers.GetLength(1) - i; j++)
+                {
+                    resultMatrix[j + i, j] = matrixNumbers[j + i, j];
+                }
             }
+
+            return resultMatrix;
+        }
+
+        private static int[,] TriangleAboveReflectedMainDiagonal(int[,] matrixNumbers)
+        {
+            int[,] resultMatrix = new int[matrixNumbers.GetLength(0), matrixNumbers.GetLength(1)];
+
+            for (int i = matrixNumbers.GetLength(0) - 1; i >= 0; i--)
+            {
+                for (int j = 0; j < (i + 1); j++)
+                {
+                    resultMatrix[j, i - j] = matrixNumbers[j, i - j];
+                }
+            }
+
+            return resultMatrix;
+        }
+
+        private static int[,] TriangleBelowReflectedMainDiagonal(int[,] matrixNumbers)
+        {
+            int[,] resultMatrix = new int[matrixNumbers.GetLength(0), matrixNumbers.GetLength(1)];
+
+            for (int i = matrixNumbers.GetLength(0) - 1; i >= 0; i--)
+            {
+                for (int j = 0; j < (i + 1); j++)
+                {
+                    if (i == 1)
+                    {
+                        resultMatrix[j + i, 2 * i - j] = matrixNumbers[j + i, 2 * i - j];
+                        continue;
+                    }
+                    else if (i == 0)
+                    {
+                        resultMatrix[2, 2] = matrixNumbers[2, 2];
+                        continue;
+                    }
+
+                    resultMatrix[i - j, j] = matrixNumbers[i - j, j];
+                }
+            }
+
+            return resultMatrix;
         }
     }
 }
---
+++ Homework_2dArrays_Zad21/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Homework_2dArrays_Zad21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int matrixRank = 0;
+
+            if (int.TryParse(Console.ReadLine(), out matrixRank) & matrixRank >= 2)
+            {
+                int numberCounter = 1;
+                bool matrixIsReflexive = true;
+                int[,] matrixReflection = new int[matrixRank, matrixRank];
+
+                Console.WriteLine("Матрица: ");
+                for (int i = 0; i < matrixRank; i++)
+                {
+                    for (int j = 0; j < matrixRank; j++)
+                    {
+                        Console.Write("Число " + numberCounter + ":");
+                        int.TryParse(Console.ReadLine(), out matrixReflection[i, j]);
+                        numberCounter++;
+                    }
+                }
+
+                for (int i = 0; i < matrixRank; i++)
+                {
+                    if (!(matrixReflection[i, i] == 1))
+                    {
+                        matrixIsReflexive = false;
+                        break;
+                    }
+                }
+
+                if (matrixIsReflexive)
+                    Console.WriteLine("Matrix is reflexive.");
+                else
+                    Console.WriteLine("Matrix is NOT reflexive!");
+            }
+            else
+            {
+                Console.WriteLine("Error!");
+            }
+        }
+    }
+}
